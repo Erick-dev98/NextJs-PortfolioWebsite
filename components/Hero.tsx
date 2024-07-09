7f03178dcc40ffbe9db85650a6a3738f505fe43a
erickmwei680@gmail.com <erickmwei680@gmail.com>
2024-07-09 13:08:06 +0300

    changed my Hero description

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
       <div className='flex justify-center relative my-20 z-10'>
         <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
           <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
-            Expert in React, Laravel, and WordPress
+            Expert in ReactJs, NextJs, and Laravel
           </h2>
 
           <TextGenerateEffect
