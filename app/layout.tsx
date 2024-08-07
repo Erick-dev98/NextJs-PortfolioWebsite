27fe3cc43cd1dc5857c20b26c3f1246023836e83
erickmwei680@gmail.com <erickmwei680@gmail.com>
2024-08-07 14:33:49 +0300

    title modification

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Erick Mutua - Software Engineer",
+  title: "Erick Mutua - Software Developer",
   description: "🌟 Welcome to my portfolio, showcasing my expertise in software development, including projects in JavaScript, React, Next.js, TypeScript, PHP, and Laravel.",
 };
 
